47d1f1f03d6770004e410a6c52b0220b15f65ae3
Ronaldo_Op <brandonprimo37@hotmail.com>
2025-08-03 18:19:02 -0600

    Sunday

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,10 @@ const supabaseUrl = "https://qdqwnodowkudmadhtgcb.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkcXdub2Rvd2t1ZG1hZGh0Z2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwODc0NzksImV4cCI6MjA2OTY2MzQ3OX0.rss09Mm_pjLH9JnmxnkzrZurYyGGir4IYamu8YtBmA8";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
